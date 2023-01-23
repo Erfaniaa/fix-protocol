fc705a6604d593dfa1e197ae614fd93814ca6c77
Erfan Alimohammadi <erfan.aa@gmail.com>
2023-01-24 00:12:34 +0100

    Add FixedString class with split method

---
+++ include/fixedstring.h
@@ -0,0 +1,41 @@
+#ifndef FIXED_STRING_H
+#define FIXED_STRING_H
+
+#include <cstddef>
+#include <cstring>
+#include <string>
+#include <vector>
+
+class FixedString {
+public:
+	FixedString();
+
+	void push_back(const char& value);
+
+	void pop_back();
+
+	std::size_t size() const;
+
+	char& operator[](std::size_t index);
+
+	const char& operator[](std::size_t index) const;
+
+	FixedString& operator=(const char* str);
+
+	void FixedString::operator=(const std::string& str);
+
+	bool operator==(const FixedString& other) const;
+
+	FixedString& operator+(const FixedString& other);
+
+	std::vector<FixedString> FixedString::split(const char& delimiter);
+
+	const char* c_str() const;
+
+private:
+	static constexpr std::size_t MaxSize = 1024;
+	std::size_t size_;
+	char data_[MaxSize + 1];
+};
+
+#endif
---
+++ src/fixedstring.cpp
@@ -0,0 +1,106 @@
+#include <cstddef>
+#include <cstring>
+#include <string>
+#include <stdexcept>
+#include "../include/fixedstring.h"
+
+FixedString::FixedString() : size_(0) {
+	data_[0] = '\0';
+}
+
+void FixedString::push_back(const char& value) {
+	if (size_ < MaxSize) {
+		data_[size_++] = value;
+		data_[size_] = '\0';
+	}
+}
+
+void FixedString::pop_back() {
+	if (size_ > 0) {
+		--size_;
+		data_[size_] = '\0';
+	}
+}
+
+std::size_t FixedString::size() const {
+	return size_;
+}
+
+char& FixedString::operator[](std::size_t index) {
+	return data_[index];
+}
+
+const char& FixedString::operator[](std::size_t index) const {
+	return data_[index];
+}
+
+FixedString& FixedString::operator=(const char* str) {
+	size_ = 0;
+	std::size_t len = std::strlen(str);
+	for (std::size_t i = 0; i < len && i < MaxSize; ++i) {
+		data_[i] = str[i];
+		++size_;
+	}
+	data_[size_] = '\0';
+	return *this;
+}
+
+void FixedString::operator=(const std::string& str) {
+	if (str.size() > MaxSize) {
+		throw std::length_error("String exceeds maximum size of FixedString");
+	}
+
+	size_ = str.size();
+	std::copy(str.begin(), str.end(), data_);
+	data_[size_] = '\0'; // Ensure null-terminated string
+}
+
+bool FixedString::operator==(const FixedString& other) const {
+	if (size_ != other.size_) {
+		return false;
+	}
+	for (std::size_t i = 0; i < size_; ++i) {
+		if (data_[i] != other.data_[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+FixedString& FixedString::operator+(const FixedString& other) {
+	if (size_ + other.size_ > MaxSize) {
+		throw std::length_error("FixedString length exceeded");
+	}
+
+	for (std::size_t i = 0; i < other.size_; ++i) {
+		data_[size_ + i] = other.data_[i];
+	}
+
+	size_ += other.size_;
+
+	return *this;
+}
+
+std::vector<FixedString> FixedString::split(const char& delimiter) {
+	std::vector<FixedString> result;
+	FixedString current;
+
+	for (std::size_t i = 0; i < size_; ++i) {
+		if (data_[i] == delimiter) {
+			result.push_back(current);
+			current = FixedString();
+		} else {
+			current.push_back(data_[i]);
+		}
+    }
+
+    if (current.size() > 0) {  // Add the last element if it exists 
+        result.push_back(current); 
+    }
+
+    return result;  // Return the vector of FixedStrings 
+}
+
+const char* FixedString::c_str() const {
+	return data_;
+}
