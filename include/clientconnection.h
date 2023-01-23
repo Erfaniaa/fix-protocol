ba08c8a703539d96f779cba204d969beb344b6b5
Erfan Alimohammadi <erfan.aa@gmail.com>
2023-01-23 10:28:04 +0100

    Add ClientConnection class header file

---
+++ include/clientconnection.h
@@ -0,0 +1,18 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <string>
+
+class ClientConnection {
+private:
+	const std::string host = "127.0.0.1";
+	const int port = 5555;
+	int newSocket = 0, clientFD = 0;
+public:
+	void openConnection();
+	void closeConnection();
+	void sendMessage(std::string message);
+	std::string receiveMessage();
+};
+
+#endif
