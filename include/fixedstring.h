ee77e5805088c61deeefe04b23f8dae98232eaa6
Erfan Alimohammadi <erfan.aa@gmail.com>
2023-01-24 05:21:48 +0100

    Remove new lines

--- include/fixedstring.h
+++ include/fixedstring.h
@@ -9,31 +9,18 @@
 class FixedString {
 public:
 	FixedString();
-
 	FixedString(const std::string& str);
-
 	FixedString(const char* str);
-
 	void push_back(const char& value);
-
 	void pop_back();
-
 	std::size_t size() const;
-
 	char& operator[](std::size_t index);
-
 	const char& operator[](std::size_t index) const;
-
 	FixedString& operator=(const char* str);
-
 	void FixedString::operator=(const std::string& str);
-
 	bool operator==(const FixedString& other) const;
-
 	FixedString& operator+(const FixedString& other);
-
 	std::vector<FixedString> FixedString::split(const char& delimiter);
-
 	const char* c_str() const;
 
 private:
