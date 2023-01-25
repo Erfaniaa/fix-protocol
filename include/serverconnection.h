886042b2db50f6194c3eb552ab30f318b152b207
Erfan Alimohammadi <erfan.aa@gmail.com>
2023-01-25 18:20:53 +0100

    Change ServerConnection

--- include/serverconnection.h
+++ include/serverconnection.h
@@ -1,17 +1,25 @@
 #ifndef SERVER_CONNECTION_H
 #define SERVER_CONNECTION_H
 
+#include <iostream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include <string>
+#include "../include/fixedstring.h"
 
 class ServerConnection {
+public:
+	void open_connection();
+	void close_connection();
+	void send_message(FixedString message);
+	FixedString receive_message();
 private:
 	const int port = 5555;
-	int newSocket = 0, serverFD = 0;
-public:
-	void openConnection();
-	void closeConnection();
-	void sendMessage(std::string message);
-	std::string receiveMessage();
+	int new_socket = 0, server_fd = 0;
 };
 
 #endif
--- src/serverconnection.cpp
+++ src/serverconnection.cpp
@@ -1,25 +1,17 @@
-#include <iostream>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <string>
 #include "../include/serverconnection.h"
 
 
-void ServerConnection::openConnection() {
+void ServerConnection::open_connection() {
 	struct sockaddr_in address;
 	int opt = 1;
-	int addressLen = sizeof(address);
+	int address_len = sizeof(address);
 
-	if ((serverFD = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		std::cout << "socket failed error" << std::endl;
 		return;
 	}
 
-	if (setsockopt(serverFD, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
 		std::cout << "setsockopt error" << std::endl;
 		return;
 	}
@@ -27,33 +19,33 @@ void ServerConnection::openConnection() {
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(port);
 
-	if (bind(serverFD, (struct sockaddr*)&address, sizeof(address)) < 0) {
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
 		std::cout << "bind failed" << std::endl;
 		return;
 	}
 
-	if (listen(serverFD, 3) < 0) {
+	if (listen(server_fd, 3) < 0) {
 		std::cout << "listen error" << std::endl;
 		return;
 	}
 
-	if ((newSocket = accept(serverFD, (struct sockaddr*)&address, (socklen_t*)&addressLen)) < 0) {
+	if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&address_len)) < 0) {
 		std::cout << "accept connection error" << std::endl;
 		return;
 	}
 }
 
-void ServerConnection::closeConnection() {
-	close(newSocket);
-	shutdown(serverFD, SHUT_RDWR);
+void ServerConnection::close_connection() {
+	close(new_socket);
+	shutdown(server_fd, SHUT_RDWR);
 }
 
-void ServerConnection::sendMessage(std::string message) {
-	send(newSocket, message.c_str(), strlen(message.c_str()), 0);
+void ServerConnection::send_message(FixedString message) {
+	send(new_socket, message.c_str(), message.size(), 0);
 }
 
-std::string ServerConnection::receiveMessage() {
+FixedString ServerConnection::receive_message() {
 	char buffer[1024] = { 0 };
-	read(newSocket, buffer, 1024);
+	read(new_socket, buffer, 1024);
 	return buffer;
 }
