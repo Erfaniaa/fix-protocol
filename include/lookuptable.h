f73e2eb2811730791f037b4296db70557c73ee2e
Erfan Alimohammadi <erfan.aa@gmail.com>
2023-01-25 23:49:34 +0100

    Refactor LookupTable for FixedString values list

--- include/lookuptable.h
+++ include/lookuptable.h
@@ -4,21 +4,21 @@
 #include <cstddef>
 #include "../include/constants.h"
 
-template <typename T>
+
 class LookupTable {
 public:
-	LookupTable(const T& default_value);
-	void insert(unsigned short key, const T& value);
-	T& operator[](unsigned short key);
-	const T& operator[](unsigned short key) const;
+	LookupTable();
+	void insert(unsigned short key, const FixedString& value);
+	FixedString& operator[](unsigned short key);
+	const FixedString& operator[](unsigned short key) const;
 	bool has_key(unsigned short key);
 	std::size_t size();
 
 private:
 	static constexpr std::size_t kMaxSize = constants::MAX_MESSAGE_LENGTH;  
 	std::size_t size_;  
-	T values_[kMaxSize];
-	T default_value_;
+	FixedString values_[kMaxSize];
+	FixedString default_value_ = "-1";
 };
 
 #endif
--- src/lookuptable.cpp
+++ src/lookuptable.cpp
@@ -1,42 +1,36 @@
 #include "../include/lookuptable.h"
 
 
-template <typename T>
-LookupTable<T>::LookupTable(const T& default_value) : size_(0), default_value_(default_value) {}
+LookupTable::LookupTable() : size_(0) {}
 
-template <typename T>
-void LookupTable<T>::insert(unsigned short key, const T& value) {
-	if (size_ < MaxSize) {
-		if (value != default_value_ && values_[key] == default_value_)
+void LookupTable::insert(unsigned short key, const FixedString& value) {
+	if (size_ < kMaxSize) {
+		if (!(value == default_value_) && values_[key] == default_value_)
 			++size_;
 		values_[key] = value;
 	}
 }
 
-template <typename T>
-T& LookupTable<T>::operator[](unsigned short key) {
-	if (key >= 0 && key < MaxSize) {
+FixedString& LookupTable::operator[](unsigned short key) {
+	if (key >= 0 && key < kMaxSize) {
 		return values_[key]; 
 	} else {  // Return default value if key is not found 
 		return default_value_; 
 	}
 }
 
-template <typename T>
-const T& LookupTable<T>::operator[](unsigned short key) const { 
-	if (key >= 0 && key < MaxSize) { 
+const FixedString& LookupTable::operator[](unsigned short key) const { 
+	if (key >= 0 && key < kMaxSize) { 
 		return values_[key]; 
 	} else { // Return default value if key is not found 
 		return default_value_; 
 	}
 }
 
-template <typename T>
-bool LookupTable<T>::has_key(unsigned short key) {
-	return table[key] != default_value_;
+bool LookupTable::has_key(unsigned short key) {
+	return !(values_[key] == default_value_);
 }
 
-template <typename T>
-std::size_t LookupTable<T>::size() {
+std::size_t LookupTable::size() {
 	return size_;
 }
