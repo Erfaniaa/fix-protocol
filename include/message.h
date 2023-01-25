64a8d497105092a938a991a3eb358c638f992cec
Erfan Alimohammadi <erfan.aa@gmail.com>
2023-01-25 23:56:58 +0100

    Add LookupTable to Message class

--- include/message.h
+++ include/message.h
@@ -1,17 +1,20 @@
 #include "constants.h"
 #include "fixedstring.h"
-#include "fixedvector.h"
+#include "lookuptable.h"
 #include "field.h"
 
-class Message
-{
+class Message {
 public:
+	Message();
 	Message(FixedString str);
 	FixedString message_type() const;
 	void add_field(const Field &field);
 	FixedString serialize() const;
+	bool has_tag(unsigned short tag);
+	FixedString get_tag_value(unsigned short tag);
 
 private:
 	FixedString message_type_;
-	FixedVector<Field> fields_;
+	LookupTable fields_lookup_table_;
+	FixedVector<Field> fields_fixed_vector_;
 };
--- src/message.cpp
+++ src/message.cpp
@@ -1,5 +1,22 @@
 #include "../include/message.h"
 
+Message::Message() {}
+
+Message::Message(FixedString str) {
+	// Split string into tokens
+	FixedVector<FixedString> tokens;
+	std::istringstream iss(str.c_str());
+	for (std::string token; std::getline(iss, token, constants::SOH[0]); ) {
+		tokens.push_back(token);
+	}
+
+	// Parse fields from remaining tokens
+	for (int i = 1; i < tokens.size(); ++i) {
+		Field field(tokens[i]);
+		add_field(field);
+  	}
+}
+
 Message::Message(FixedString str) {
 	// Split string into tokens
 	FixedVector<FixedString> tokens;
@@ -23,13 +40,22 @@ FixedString Message::message_type() const {
 }
 
 void Message::add_field(const Field& field) {
-	fields_.push_back(field);
+	fields_fixed_vector_.push_back(field);
+	fields_lookup_table_[field.tag()] = field.value();
 }
 
 FixedString Message::serialize() const {
 	FixedString serialized_message;
-	for (unsigned short i = 0; i < fields_.size(); i++) {
+	for (unsigned short i = 0; i < fields_fixed_vector_.size(); i++) {
 		serialized_message = serialized_message + FixedString(std::to_string(fields_[i].tag())) + FixedString("=") + fields_[i].value() + constants::SOH;
 	}
 	return serialized_message;
+}
+
+bool Message::has_tag(unsigned short tag) {
+	return fields_lookup_table_.has_key(tag);	
+}
+
+FixedString Message::get_tag_value(unsigned short tag) {
+	return fields_lookup_table_[tag];
 }
