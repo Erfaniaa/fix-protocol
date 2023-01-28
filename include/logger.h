9a2c5e2561c83ac46a1dce35ca531eb9a940c32a
Erfan Alimohammadi <erfan.aa@gmail.com>
2023-01-29 00:39:01 +0100

    Make Logger's methods static

--- include/logger.h
+++ include/logger.h
@@ -7,12 +7,12 @@ class Logger {
 public:
 	Logger();
 	~Logger();
-	void log(std::string str);
-	void log(char* s);
-	void log_error(std::string str);
-	void log_error(char* s);
-	void log_info(std::string str);
-	void log_info(char* s);
-	void log_warning(std::string str);
-	void log_warning(char* s);
+	static void log(std::string str);
+	static void log(char* s);
+	static void log_error(std::string str);
+	static void log_error(char* s);
+	static void log_info(std::string str);
+	static void log_info(char* s);
+	static void log_warning(std::string str);
+	static void log_warning(char* s);
 };
