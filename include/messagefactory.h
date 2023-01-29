d43947f5736546908aacfe18a360a999752c323d
Erfan Alimohammadi <erfan.aa@gmail.com>
2023-01-29 02:02:36 +0100

    Convert MessageFactory method names to snake_case

--- include/messagefactory.h
+++ include/messagefactory.h
@@ -5,16 +5,16 @@
 class MessageFactory {
 public:
 	MessageFactory();
-	static Message* MessageFactory::CreateLogon();
-	static Message* MessageFactory::CreateLogout();
-	static Message* MessageFactory::CreateHeartbeat();
-	static Message* MessageFactory::CreateTestRequest();
-	static Message* MessageFactory::CreateResendRequest(unsigned short begin_seq_num, unsigned short end_seq_num);
-	static Message* MessageFactory::CreateReject(unsigned short ref_seq_num, FixedString ref_tag_id, FixedString ref_msg_type);
-	static Message* MessageFactory::CreateNewOrderSingle(FixedString clordid, FixedString symbol, FixedString side, int orderQty, 
+	static Message* MessageFactory::create_logon();
+	static Message* MessageFactory::create_logout();
+	static Message* MessageFactory::create_heartbeat();
+	static Message* MessageFactory::create_test_request();
+	static Message* MessageFactory::create_resend_request(unsigned short begin_seq_num, unsigned short end_seq_num);
+	static Message* MessageFactory::create_reject(unsigned short ref_seq_num, FixedString ref_tag_id, FixedString ref_msg_type);
+	static Message* MessageFactory::create_new_order_single(FixedString clordid, FixedString symbol, FixedString side, int orderQty, 
 														 FixedString ordType, double price, FixedString timeInForce, 
-										    			 FixedString orderCapacity, FixedString orderRestattribs);
-	static Message* MessageFactory::CreateSequenceReset(unsigned short new_seq_num, bool gap_fill_flag);
-	static Message* MessageFactory::CreateOrderCancelRequest(unsigned short orig_clord_id, unsigned short order_id, unsigned short symbol, unsigned short side);
-	static Message* MessageFactory::CreateOrderCancelReject(unsigned short ref_seq_num, FixedString clord_id, FixedString orig_clord_id, FixedString text);
+														 FixedString orderCapacity, FixedString orderRestattribs);
+	static Message* MessageFactory::create_sequence_reset(unsigned short new_seq_num, bool gap_fill_flag);
+	static Message* MessageFactory::create_order_cancel_request(unsigned short orig_clord_id, unsigned short order_id, unsigned short symbol, unsigned short side);
+	static Message* MessageFactory::create_order_cancel_reject(unsigned short ref_seq_num, FixedString clord_id, FixedString orig_clord_id, FixedString text);
 };
--- src/messagefactory.cpp
+++ src/messagefactory.cpp
@@ -3,7 +3,7 @@
 
 MessageFactory::MessageFactory() {}
 
-Message* MessageFactory::CreateLogon() {
+Message* MessageFactory::create_logon() {
 	Message* msg = new Message();
 	msg->add_field(constants::ENCRYPT_METHOD, "0");
 	msg->add_field(constants::HEART_BT_INT, "30");
@@ -13,31 +13,31 @@ Message* MessageFactory::CreateLogon() {
 	return msg;
 }
 
-Message* MessageFactory::CreateLogout() {
+Message* MessageFactory::create_logout() {
 	Message* msg = new Message();
 	msg->add_field(constants::TEXT, "Closing connection");
 	return msg;
 }
 
-Message* MessageFactory::CreateHeartbeat() {
+Message* MessageFactory::create_heartbeat() {
 	Message* msg = new Message();
 	return msg;
 }
 
-Message* MessageFactory::CreateTestRequest() {
+Message* MessageFactory::create_test_request() {
 	Message* msg = new Message();
 	msg->add_field(constants::TEST_REQ_ID, "Test Request ID");
 	return msg;
 }
 
-Message* MessageFactory::CreateResendRequest(unsigned short begin_seq_num, unsigned short end_seq_num) {
+Message* MessageFactory::create_resend_request(unsigned short begin_seq_num, unsigned short end_seq_num) {
 	Message* msg = new Message();
 	msg->add_field(constants::BEGIN_SEQ_NUM, std::to_string(begin_seq_num));
 	msg->add_field(constants::END_SEQ_NUM, std::to_string(end_seq_num));
 	return msg;
 }
 
-Message* MessageFactory::CreateReject(unsigned short ref_seq_num, FixedString ref_tag_id, FixedString ref_msg_type) {
+Message* MessageFactory::create_reject(unsigned short ref_seq_num, FixedString ref_tag_id, FixedString ref_msg_type) {
 	Message* msg = new Message();
 	msg->add_field(constants::REF_SEQ_NUM, std::to_string(ref_seq_num));
 	msg->add_field(constants::REF_TAG_ID, ref_tag_id);
@@ -45,9 +45,9 @@ Message* MessageFactory::CreateReject(unsigned short ref_seq_num, FixedString re
 	return msg;
 }
 
-Message* MessageFactory::CreateNewOrderSingle(FixedString clordid, FixedString symbol, FixedString side, int orderQty, 
-											  FixedString ordType, double price, FixedString timeInForce, 
-										      FixedString orderCapacity, FixedString orderRestattribs) {
+Message* MessageFactory::create_new_order_single(FixedString clordid, FixedString symbol, FixedString side, int orderQty, 
+												 FixedString ordType, double price, FixedString timeInForce, 
+										    	 FixedString orderCapacity, FixedString orderRestattribs) {
     Message* msg = new Message();
 	msg->add_field(constants::MSG_TYPE, constants::NEW_ORDER_SINGLE);
 	msg->add_field(constants::CLORDID, clordid);
@@ -62,7 +62,7 @@ Message* MessageFactory::CreateNewOrderSingle(FixedString clordid, FixedString s
     return msg;
 }
 
-Message* MessageFactory::CreateSequenceReset(unsigned short new_seq_num, bool gap_fill_flag) {
+Message* MessageFactory::create_sequence_reset(unsigned short new_seq_num, bool gap_fill_flag) {
 	Message* msg = new Message();
 	msg->add_field(constants::MSG_TYPE, constants::SEQUENCE_RESET);
 	msg->add_field(constants::NEW_SEQ_NUM, std::to_string(new_seq_num));
@@ -72,7 +72,7 @@ Message* MessageFactory::CreateSequenceReset(unsigned short new_seq_num, bool ga
 	return msg;
 }
 
-Message* MessageFactory::CreateOrderCancelRequest(unsigned short orig_clord_id, unsigned short order_id, unsigned short symbol, unsigned short side) {
+Message* MessageFactory::create_order_cancel_request(unsigned short orig_clord_id, unsigned short order_id, unsigned short symbol, unsigned short side) {
 	Message* msg = new Message();
 	msg->add_field(constants::MSG_TYPE, constants::ORDER_CANCEL_REQUEST);
 	msg->add_field(constants::ORIG_CLORD_ID, std::to_string(orig_clord_id));
@@ -82,7 +82,7 @@ Message* MessageFactory::CreateOrderCancelRequest(unsigned short orig_clord_id, 
 	return msg;
 }
 
-Message* MessageFactory::CreateOrderCancelReject(unsigned short ref_seq_num, FixedString clord_id, FixedString orig_clord_id, FixedString text) {
+Message* MessageFactory::create_order_cancel_reject(unsigned short ref_seq_num, FixedString clord_id, FixedString orig_clord_id, FixedString text) {
 	Message* msg = new Message();
 	msg->add_field(constants::REF_SEQ_NUM, std::to_string(ref_seq_num));
 	msg->add_field(constants::CLORDID, clord_id);
