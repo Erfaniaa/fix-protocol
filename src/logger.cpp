bd65de027bad3d389e2de436e9164f6320f8e05d
Roozbeh <roozbehsayadi@gmail.com>
2023-02-02 19:01:35 +0330

    Use constexpr for boolean flags

--- src/logger.cpp
+++ src/logger.cpp
@@ -6,41 +6,41 @@ Logger::Logger() {}
 Logger::~Logger() {}
 	
 void Logger::log(std::string str) {
-	if (constants::LOG_GENERAL_MESSAGES)
+	if constexpr (constants::LOG_GENERAL_MESSAGES)
 		std::cout << str << std::endl;
 }
 
 void Logger::log(char* s) {
-	if (constants::LOG_GENERAL_MESSAGES)
+	if constexpr (constants::LOG_GENERAL_MESSAGES)
 		std::cout << s << std::endl;
 }
 	
 void Logger::log_error(std::string str) {
-	if (constants::LOG_ERRORS)
+	if constexpr (constants::LOG_ERRORS)
 		std::cout << "Error: " << str << std::endl;
 }
 
 void Logger::log_error(char* s) {
-	if (constants::LOG_ERRORS)
+	if constexpr (constants::LOG_ERRORS)
 		std::cout << "Error: " << s << std::endl;
 }
 	
 void Logger::log_info(std::string str) {
-	if (constants::LOG_INFO)
+	if constexpr (constants::LOG_INFO)
 		std::cout << "Info: " << str << std::endl;
 }
 
 void Logger::log_info(char* s) {
-	if (constants::LOG_INFO)
+	if constexpr (constants::LOG_INFO)
 		std::cout << "Info: " << s << std::endl;
 }
 	
 void Logger::log_warning(std::string str) {
-	if (constants::LOG_WARNINGS)
+	if constexpr (constants::LOG_WARNINGS)
 		std::cout << "Warning: " << str << std::endl;
 }
 
 void Logger::log_warning(char* s) {
-	if (constants::LOG_WARNINGS)
+	if constexpr (constants::LOG_WARNINGS)
 		std::cout << "Warning: " << s << std::endl;
 }
